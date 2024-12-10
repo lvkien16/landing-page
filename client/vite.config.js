15c69168b4ade6d74977b989327d824339f6b340
lvkien16 <lovankien@gmail.com>
2024-12-10 22:38:01 +0700

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false
-      },
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000", // Backend cục bộ
+                changeOrigin: true,
+                secure: false,
+            },
+        },
     },
-  },
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        outDir: "dist", // Thư mục output (mặc định là 'dist')
+    },
+});
